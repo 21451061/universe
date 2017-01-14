f28d9dcbf66737b1468fde6d2e0cefb0b60bf1ca
Jonathan Gray <jgray@openai.com>
2017-01-13 17:59:15 -0800

    env stress tests: disable_hyperthreads param

--- env_stress_tests/run/make-podspec.py
+++ env_stress_tests/run/make-podspec.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python
 
-def generate_podspec(env_id, cpu_quota, recorder_logdir):
+def generate_podspec(env_id, cpu_quota, recorder_logdir, disable_hyperthreads="true"):
     with open('pod.yaml.template', 'r') as f:
         y = f.read()
         y = y.replace('__ENV_ID__', env_id)
         y = y.replace('__CPU_QUOTA__', cpu_quota)
         y = y.replace('__RECORDER_LOGDIR__', recorder_logdir)
+        y = y.replace('__DISABLE_HYPERTHREADS__', str(disable_hyperthreads).lower())
     return y
 
 
 if __name__ == '__main__':
     import sys
-    if len(sys.argv) != 4:
-        print('Usage: {} <env_id> <cpu_quota> <recorder_logdir>'.format(sys.argv[0]))
-        print('Got: {}'.format(sys.argv))
+    if len(sys.argv) not in (4, 5):
+        print('Usage: {} <env_id> <cpu_quota> <recorder_logdir> [disable_hyperthreads]'.format(sys.argv[0]))
+        print('Got: {}'.format(' '.join(sys.argv)))
         sys.exit(1)
-    print(generate_podspec(sys.argv[1], sys.argv[2], sys.argv[3]))
+    print(generate_podspec(*sys.argv[1:]))
